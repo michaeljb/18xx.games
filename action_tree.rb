7dfe00bb556ec691eb9004608afb81fdec9d0c02
Michael Brandt <michaelbrandt5@gmail.com>
2023-09-23 16:40:06 -0600

    action tree prototype looks good, handling undos/redos, can print with or without them
    next TODO - use the action tree, make it possible to load up a branch; every
    action id or just branch id? note that stepping through history will still want
    just the next action (link to child whose branch matches the current action's branch)

--- action_tree.rb
+++ action_tree.rb
@@ -12,6 +12,8 @@ filename = 'hs_ypmxafwv_1695321744.json'  # new 1846 2p hotseat
 
 @game = Engine::Game.load(filename)
 
+@undoing_branch = nil
+@active_undos = []
 
 # @actions is a Hash; contains all actions except messages
 #   key: action id
@@ -24,6 +26,7 @@ filename = 'hs_ypmxafwv_1695321744.json'  # new 1846 2p hotseat
 # branches which were abandoned via undos
 prev_action = nil
 branch_num = 0
+@active_branch = 0
 @actions = @game.raw_actions.each_with_object({}) do |original_action, action_tree|
   # dup to avoid bad mutations on actual game state
   action = original_action.dup
@@ -34,41 +37,70 @@ branch_num = 0
   id = action['id']
 
   # tmp, look at only first N actions
-  next if id > 14
+  # n = 28
+  # next if id > n
 
   # add current action to the tree, linking it to its parent action
-  action['branch'] = 0
-  action['children'] = []
-  action['parent'] = nil
-  if prev_action
-    action['parent'] = prev_action['id']
-    prev_action['children'] << id
+  if action['type'] != 'redo'
+    action['branch'] = 0
+    action['children'] = []
+    action['parent'] = nil
+    if prev_action
+      action['parent'] = prev_action['id']
+      prev_action['children'] << id
+    end
   end
   action_tree[id] = action
 
-  if action['type'] == 'undo'
+  case action['type']
+  when 'undo'
+    @active_undos << action['id']
+
     # id of last action which was not undone
     prev_id = action['action_id'] || prev_action['parent']
 
-    # undoing creates a new branch
+    # undoing creates a new branch; undoing multiple actions consecutively only
+    # creates branches for the undo actions
     branch_num += 1
     branch = branch_num
+    unless @undoing_branch
+      @undoing_branch = branch_num
+    end
     action['branch'] = branch
 
-    # TODO: track active undos; if undoing multiple things they should all be on
-    # one branch, only the undos need their own unique branches
-    #
     # move undone actions to new branch; actions which are already branched
     # don't need to branch again
     parent_id = action['parent']
     until parent_id == prev_id
       parent_action = action_tree[parent_id]
-      parent_action['branch'] = branch
+      parent_action['branch'] = @undoing_branch
       parent_id = parent_action['parent']
     end
 
     prev_action = action_tree[prev_id]
+  when 'redo'
+    undo_id = @active_undos.pop
+    @undoing_branch = nil if @active_undos.empty?
+
+    # redo is a special case, the only action which can be the child of an undo
+    action['parent'] = undo_id
+    action['children'] = []
+    undo_action = action_tree[undo_id]
+    undo_action['children'] << action['id']
+    undo_branch = undo_action['branch']
+    action['branch'] = undo_branch
+
+    # un-branch all actions on the branch that was created by the undo action
+    parent_action = action_tree[undo_action['parent']]
+    until parent_action['branch'] == 0
+      parent_action['branch'] = @active_branch
+      parent_action = action_tree[parent_action['parent']]
+    end
+
+    prev_action = action_tree[undo_action['parent']]
   else
+    @active_undos.clear
+    @undoing_branch = nil
     prev_action = action
   end
 end
@@ -95,7 +127,7 @@ end
 
 @len = @actions.keys.max.to_s.size
 
-def print_action_t(action_tree, a, level: 0)
+def print_action_t(action_tree, a, level: 0, show_undo: false)
   len = @len
 
   action_str =
@@ -105,16 +137,26 @@ def print_action_t(action_tree, a, level: 0)
       a['type']
     end
 
-  #unless %w[undo redo].include?(a['type'])
-  puts "b:%2s   "\
-       " #{'  ' * level}%#{len}s   "\
-       "p:%#{len}s   "\
-       "c:%-#{(2*len)+4}s   "\
-       "e:#{a['entity']}   "\
-       "a:#{action_str}"\
-       "" % [a['branch'], a['id'], a['parent'], a['children'].to_s]
+  should_render =
+    if %w[undo redo].include?(a['type'])
+      show_undo
+    else
+      true
+    end
 
-  a['children'].sort_by { |id| action_tree[id]['branch'] == a['branch'] ? 1 : 0 }.each do |id|
+  if should_render
+    puts "b:%2s   "\
+         " #{'  ' * level}%#{len}s   "\
+         "p:%#{len}s   "\
+         "c:%-#{(2*len)+4}s   "\
+         "e:#{a['entity']}   "\
+         "a:#{action_str}"\
+         "" % [a['branch'], a['id'], a['parent'], a['children'].to_s]
+  end
+
+  puts "\n" if show_undo && a['children'] && a['children'].empty?
+
+  (a['children'] || []).sort_by { |id| action_tree[id]['branch'] == a['branch'] ? 1 : 0 }.each do |id|
     child = action_tree[id]
 
     l = level +
@@ -123,44 +165,15 @@ def print_action_t(action_tree, a, level: 0)
         else
           0
         end
-    print_action_t(action_tree, child, level: l)
+    print_action_t(action_tree, child, level: l, show_undo:show_undo)
   end
 
 
   # don't show a return value in irb console
   nil
-end
 
-# this is crap
-def print_action_crap(action, level: 0)
-  prefix =
-    if level.zero?
-      ''
-    else
-      "%4s #{' ' * (level-1)}" % '|'
-    end
-
-  puts "#{prefix}%4s -- p:#{action['parent']} -- c:#{action['children'].to_s} -- "\
-       "#{action['entity']}: #{action['type']}" % action['id']
-
-  case action['children'].size
-  when 1
-    puts "#{' ' * level}   |"
-    next_action = @actions[action['children'].first]
-    print_action(next_action, level: level)
-  when 2
-    puts "#{' ' * level}   |\\"
-    puts "#{' ' * level}   | \\"
-    puts "#{' ' * level}   |  \\"
-    puts "#{' ' * level}   |   \\"
-
-    next_action = @actions[action['children'].last]
-    print_action(next_action, level: level + 1)
-
-    next_action = @actions[action['children'].first]
-    print_action(next_action, level: level)
+  if a['id'] == 1
+    puts "\nactive_undos = #{@active_undos}\n\n"
+    puts "\nundoing_branch = #{@undoing_branch}\n\n"
   end
-
-  # don't show a return value in irb console
-  nil
 end
