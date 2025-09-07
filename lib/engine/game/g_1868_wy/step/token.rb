942ce79a1f8322299b5eb9c7e727c33e889ee751
Michael Brandt <michaelbrandt5@gmail.com>
2025-09-07 17:47:42 -0600

    [1868WY] fix behavior when DPR has no token
    DPR has no predetermined home. It is possible that a BUST round returns all of
    its tokens to the charter.
    
    Old behavior: the DPR president could choose a new home as a SR action
    
    New behavior: DPR may choose a new home on its token placement step during an
    OR. If no cities have an open slot for a token, DPR may lay/upgrade a tile, and
    then must place a token on that tile. Fixes #11301
    
    Also, move 1868 Wyoming tests out of game_state_spec to its own file. Also adds
    a new custom rspec matcher, a new file to hold all custom matchers. #11973

--- lib/engine/game/g_1868_wy/step/token.rb
+++ lib/engine/game/g_1868_wy/step/token.rb
@@ -9,6 +9,51 @@ module Engine
       module Step
         class Token < Engine::Step::Token
           include G1868WY::SkipCoalAndOil
+
+          def help
+            return unless @game.tokenless_dpr?(current_entity)
+
+            if @round.num_laid_track.positive?
+              'DPR must place its new home token in the newly opened city.'
+            else
+              'DPR may place its new home token in any open city.'
+            end
+          end
+
+          def actions(entity)
+            if @game.tokenless_dpr?(entity)
+              @round.num_laid_track.positive? ? ['place_token'] : %w[place_token pass]
+            else
+              super
+            end
+          end
+
+          def can_place_token?(entity)
+            @game.tokenless_dpr?(entity) ? true : super
+          end
+
+          def check_connected(entity, _city, hex)
+            if @game.tokenless_dpr?(entity)
+              !hex.tile.paths.empty? && hex.tile.available_slot?
+            else
+              super
+            end
+
+            @game.tokenless_dpr?(entity) ? true : super
+          end
+
+          def tokener_available_hex(entity, hex)
+            if @game.tokenless_dpr?(entity)
+              !hex.tile.paths.empty? && hex.tile.available_slot?
+            else
+              super
+            end
+          end
+
+          def process_place_token(action)
+            action.entity.coordinates = action.city.hex.id if @game.tokenless_dpr?(action.entity)
+            super
+          end
         end
       end
     end
