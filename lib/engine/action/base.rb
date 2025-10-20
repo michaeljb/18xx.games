ddfddb8f7dbaf17bcca987f96432b39579b1e06d
Michael Brandt <michaelbrandt5@gmail.com>
2025-10-19 21:33:30 -0600

    [core] raise `ActionError` if required Action kwargs get `nil` value
    replaces `undefined method _ for nil` with new `ActionError`
    
    While validating locally for #12086 and #12095, I happened to have local
    games broken by #12073 (they are pinned in prod), and the error was very
    unhelpful:
    
    `#<NoMethodError: undefined method `id' for nil:NilClass>"`
    
    Additionally, `broken_action.to_h` caused its own error in the validate
    script, as the error was coming from the Action -> hash conversion.
    
    With these changes, the error is much more helpful, making it much easier to
    identify the source of the breakage:
    
    `#<Engine::ActionError: Cannot create Engine::Action::LayTile, h_to_args() returned nil :tile from action 745>`

--- lib/engine/action/base.rb
+++ lib/engine/action/base.rb
@@ -14,18 +14,10 @@ module Engine
       def self.from_h(h, game)
         entity = game.get(h['entity_type'], h['entity']) || Player.new(nil, h['entity'])
 
-        kwargs = h_to_args(h, game)
-        required_kwargs = instance_method(:initialize).parameters.filter_map do |kind, name|
-          kind == :keyreq && name
-        end
-        required_kwargs.each do |arg|
-          if kwargs[arg].nil?
-            raise ActionError,
-                  "Cannot create #{name}, h_to_args() returned nil :#{arg} from action #{h['id']}"
-          end
-        end
+        args = h_to_args(h, game)
+        validate_args!(args)
 
-        obj = new(entity, **kwargs)
+        obj = new(entity, **args)
         obj.user = h['user'] if entity.player && h['user'] != entity.player&.id
         obj.created_at = h['created_at'] || Time.now
         obj.auto_actions = (h['auto_actions'] || []).map { |auto_h| Base.action_from_h(auto_h, game) }
@@ -40,6 +32,18 @@ module Engine
 
       def self.h_to_args(_h, _game)
         {}
+      end
+
+      def self.validate_args!(args)
+        required_args = (instance_method(:initialize).parameters || []).filter_map do |kind, name|
+          kind == :keyreq && name
+        end
+        required_args.each do |arg|
+          if args[arg].nil?
+            raise ActionError,
+              "Cannot create #{name}, h_to_args() returned nil :#{arg} from action #{h['id']}"
+          end
+        end
       end
 
       def self.split(klass)
