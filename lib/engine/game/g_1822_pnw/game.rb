73522a0df57f1586580d44c0f7fe3bea0be726ab
Michael Brandt <michaelbrandt5@gmail.com>
2025-10-23 19:16:02 -0600

    [core/1822PNW] fix Rockport coal token getting E-train revenue
    * core change: add parameter to `City#tokened_by?` to specify acceptable types
      of tokens; by default, all are valid
    * add `type: coal` to the Rockport coal token
    * in base 1822 code, pass `[:normal, :destination]` as acceptable tokens so that
      the coal token is excluded
    
    Fixes #11139

--- lib/engine/game/g_1822_pnw/game.rb
+++ lib/engine/game/g_1822_pnw/game.rb
@@ -260,7 +260,11 @@ module Engine
         end
 
         def coal_token
-          @coal_token ||= Engine::Token.new(nil, logo: '/icons/18_usa/mine.svg')
+          @coal_token ||= Engine::Token.new(nil, logo: '/icons/18_usa/mine.svg', type: :coal)
+        end
+
+        def city_tokened_by?(city, entity)
+          city.tokened_by?(entity, types: %i[normal destination])
         end
 
         def backroom_company?(entity)
@@ -482,7 +486,7 @@ module Engine
             G1822PNW::Step::Track,
             G1822::Step::DestinationToken,
             G1822::Step::Token,
-            G1822PNW::Step::Route,
+            G1822::Step::Route,
             G1822PNW::Step::Dividend,
             G1822::Step::BuyTrain,
             G1822PNW::Step::MinorAcquisition,
